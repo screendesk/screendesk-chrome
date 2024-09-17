e924f7d50ced76a19a7107bfe6b1244ca9725fb6
Adrien Nhem <adriennhem@users.noreply.github.com>
2024-09-17 13:18:26 +0100

    fix bug: handle permission error

--- src/pages/Permissions/Permissions.jsx
+++ src/pages/Permissions/Permissions.jsx
@@ -43,10 +43,11 @@ const Recorder = () => {
           {
             type: "screendesk-permissions",
             success: false,
-            error: err.name,
+            error: "Permissions not granted",
           },
           "*"
         );
+        throw new Error("Permissions not granted");
         // sendResponse({ success: false, error: err.name });
       }
     } catch (err) {
